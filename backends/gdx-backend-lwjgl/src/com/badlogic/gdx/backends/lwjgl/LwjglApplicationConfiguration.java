310bed7da906ed5c7e73831d09ac8838d7dfcbfd
Frosty-J <james@elite-20.04>
2021-07-31 20:48:17 +0100

    UIUtils for Preferences checks

--- backends/gdx-backend-lwjgl/src/com/badlogic/gdx/backends/lwjgl/LwjglApplicationConfiguration.java
+++ backends/gdx-backend-lwjgl/src/com/badlogic/gdx/backends/lwjgl/LwjglApplicationConfiguration.java
@@ -25,6 +25,7 @@ import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.Graphics.DisplayMode;
 import com.badlogic.gdx.LifecycleListener;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.utils.UIUtils;
 import com.badlogic.gdx.utils.Array;
 
 import java.awt.GraphicsDevice;
@@ -186,64 +187,52 @@ public class LwjglApplicationConfiguration {
 		return modes.toArray(new DisplayMode[modes.size()]);
 	}
 
-	private enum OperatingSystem {
-		WINDOWS, MAC, LINUX, UNKNOWN
-	}
-
 	/**
 	 * Returns where preferences are stored by default.
 	 * Typically AppData/Roaming on Windows, Library/Preferences on macOS and .config on Linux.
 	 * @return The default preferences directory.
 	 */
 	public String getDefaultPreferencesDirectory() {
-		switch (getOS()) {
 
-			case WINDOWS:
-				String appdata = System.getenv("APPDATA");
-				String windir = System.getenv("WINDIR");
-				return (appdata != null) ? appdata // 2000/XP/Vista/7/8/10/11
-					: (windir != null) ? windir + "/Application Data" // 95/98/Me
-					: preferencesBackupDirectory; // Default to backup directory (probably ~/.prefs/) if it's broken
+		if (UIUtils.isWindows) {
+			String appdata = System.getenv("APPDATA");
+			String windir = System.getenv("WINDIR");
+			return (appdata != null) ? appdata // 2000/XP/Vista/7/8/10/11
+				: (windir != null) ? windir + "/Application Data" // 95/98/Me
+				: preferencesBackupDirectory; // Default to backup directory (probably ~/.prefs/) if it's broken
 
-			case MAC:
-				return "Library/Preferences";
+		} else if (UIUtils.isMac) {
+			return "Library/Preferences";
 
-			case LINUX:
-				String configHome = System.getenv("XDG_CONFIG_HOME");
-				if (configHome != null) {
-					//TODO: Someone with regex skills should make this work with all environment variables
-					if (configHome.contains("$HOME")) configHome = configHome.replace("$HOME", System.getenv("HOME"));
-					else if (configHome.contains("$")) configHome = null;
-				}
-				return (configHome != null) ? configHome : ".config";
+		} else if (UIUtils.isLinux) {
+			String configHome = System.getenv("XDG_CONFIG_HOME");
+			if (configHome != null) {
+				//TODO: Someone with regex skills should make this work with all environment variables
+				if (configHome.contains("$HOME")) configHome = configHome.replace("$HOME", System.getenv("HOME"));
+				else if (configHome.contains("$")) configHome = null;
+			}
+			return (configHome != null) ? configHome : ".config";
 
-			default:
-				return preferencesBackupDirectory;
+		} else return preferencesBackupDirectory;
 
-		}
 	}
 
 	/**
 	 * @return The default FileType for the operating system - External or Absolute.
 	 */
 	public Files.FileType getDefaultPreferencesFileType() {
-		OperatingSystem os = getOS();
-		switch (os) {
-			case WINDOWS:
-				return (System.getenv("APPDATA") != null || System.getenv("WINDIR") != null)
-					? Files.FileType.Absolute : preferencesBackupFileType;
-			case MAC: return Files.FileType.External;
-			case LINUX: return (System.getenv("XDG_CONFIG_HOME") != null) ? Files.FileType.Absolute : Files.FileType.External;
-		}
-		return Files.FileType.External;
-	}
 
-	private OperatingSystem getOS() {
-		String osName = System.getProperty("os.name").toLowerCase(Locale.US);
-		if (osName.contains("windows")) return OperatingSystem.WINDOWS;
-		else if (osName.contains("mac")) return OperatingSystem.MAC;
-		else if (osName.contains("linux")) return OperatingSystem.LINUX;
-		else return OperatingSystem.UNKNOWN;
+		if (UIUtils.isWindows) {
+			return (System.getenv("APPDATA") != null || System.getenv("WINDIR") != null)
+				? Files.FileType.Absolute : preferencesBackupFileType;
+
+		} else if (UIUtils.isMac) {
+			return Files.FileType.External;
+
+		} else if (UIUtils.isLinux) {
+			return (System.getenv("XDG_CONFIG_HOME") != null) ? Files.FileType.Absolute : Files.FileType.External;
+		} else return Files.FileType.External;
+
 	}
 
 }
